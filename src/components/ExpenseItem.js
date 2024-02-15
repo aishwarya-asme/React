af4b25fcf238a97bcda1f80b362740f8ff30d71b
aishwarya-asme <aishwarya129jadhav@gmail.com>
2024-02-15 17:48:12 +0530

    added expense items

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem()
+{
+    
+    return (
+        <div>
+             <div>food Rs10</div>
+             <div> Petrol Rs 100</div>
+             <div>Movies Rs 200</div >
+        </div>
+    );
+}
+
+export default ExpenseItem;
